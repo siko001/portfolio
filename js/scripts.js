353db39dd6e888ff67f06b8c06f7d1ed32a2dc92
Neil <neil.mallia1@gmail.com>
2023-10-09 11:06:13 +0200

    updated portfolio with some sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,116 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+ */
 //
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', (event) => {
+	// Function to toggle section visibility
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+	function toggleSection(sectionId) {
+		const section = document.getElementById(sectionId);
+		const sectionTitle = document.querySelector(`[data-section="${sectionId}"]`);
+		const arrowDown = sectionTitle.querySelector('.fa-arrow-down');
+		const arrowUp = sectionTitle.querySelector('.fa-arrow-up');
 
-    };
+		if (section.style.display === 'none' || section.style.display === '') {
+			section.style.display = 'block';
+			arrowDown.style.display = 'inline';
+			arrowUp.style.display = 'none';
+		} else {
+			section.style.display = 'none';
+			arrowDown.style.display = 'none';
+			arrowUp.style.display = 'inline';
+		}
+	}
 
-    // Shrink the navbar 
-    navbarShrink();
+	// Initial setup: Hide all portfolio sections
+	const portfolioSections = document.querySelectorAll('.portfolio-section');
+	portfolioSections.forEach(function (section) {
+		section.style.display = 'none';
+	});
+	const sectionTitles = document.querySelectorAll('.section-title');
+	sectionTitles.forEach(function (title) {
+		title.addEventListener('click', function () {
+			const sectionId = title.getAttribute('data-section');
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+			// Toggle the section
+			toggleSection(sectionId);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
+			// Remove 'active' class from all section titles
+			sectionTitles.forEach(function (otherTitle) {
+				if (otherTitle !== title) {
+					otherTitle.classList.remove('active');
+				}
+			});
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+			// Add 'active' class to the clicked section title
+			title.classList.toggle('active');
+		});
+	});
+	// Add hover effect for section titles
+	sectionTitles.forEach(function (title) {
+		title.addEventListener('mouseenter', function () {
+			const sectionId = title.getAttribute('data-section');
+			const section = document.getElementById(sectionId);
+			if (section.style.display === 'none' || section.style.display === '') {
+				const arrowDown = title.querySelector('.fa-arrow-down');
+				arrowDown.style.display = 'inline';
+				const arrowUp = title.querySelector('.fa-arrow-up');
+				arrowUp.style.display = 'none';
+			}
+		});
 
+		title.addEventListener('mouseleave', function () {
+			const sectionId = title.getAttribute('data-section');
+			const section = document.getElementById(sectionId);
+			if (section.style.display === 'none' || section.style.display === '') {
+				const arrowDown = title.querySelector('.fa-arrow-down');
+				arrowDown.style.display = 'none';
+				const arrowUp = title.querySelector('.fa-arrow-up');
+				arrowUp.style.display = 'inline';
+			}
+		});
+	});
+	// Navbar shrink function
+	var navbarShrink = function () {
+		const navbarCollapsible = document.body.querySelector('#mainNav');
+		if (!navbarCollapsible) {
+			return;
+		}
+		if (window.scrollY === 0) {
+			navbarCollapsible.classList.remove('navbar-shrink');
+		} else {
+			navbarCollapsible.classList.add('navbar-shrink');
+		}
+	};
+
+	// Shrink the navbar
+	navbarShrink();
+
+	// Shrink the navbar when page is scrolled
+	document.addEventListener('scroll', navbarShrink);
+
+	// Activate Bootstrap scrollspy on the main nav element
+	const mainNav = document.body.querySelector('#mainNav');
+	if (mainNav) {
+		new bootstrap.ScrollSpy(document.body, {
+			target: '#mainNav',
+			rootMargin: '0px 0px -40%',
+		});
+	}
+
+	// Collapse responsive navbar when toggler is visible
+	const navbarToggler = document.body.querySelector('.navbar-toggler');
+	const responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
+	responsiveNavItems.map(function (responsiveNavItem) {
+		responsiveNavItem.addEventListener('click', () => {
+			if (window.getComputedStyle(navbarToggler).display !== 'none') {
+				navbarToggler.click();
+			}
+		});
+	});
 });
